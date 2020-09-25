dd76fb5acbda19420ae3722b74afd1f23f792c4f
Conor Barnes <conorbarnes93@gmail.com>
2020-09-25 08:16:14 -0700

    Updates

---
+++ precache-manifest.2a6dc1cc03705baed69e67aca6b22e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc9f619961b9262a687e97388bc7884",
+    "url": "/Battleship/index.html"
+  },
+  {
+    "revision": "3827adbf5dd74ca096e7",
+    "url": "/Battleship/static/css/main.a96803d1.chunk.css"
+  },
+  {
+    "revision": "90ddb696207155b3ee1a",
+    "url": "/Battleship/static/js/2.5ef5a745.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Battleship/static/js/2.5ef5a745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3827adbf5dd74ca096e7",
+    "url": "/Battleship/static/js/main.0aca5ffd.chunk.js"
+  },
+  {
+    "revision": "73bfc92df9a82ac5645c",
+    "url": "/Battleship/static/js/runtime-main.942c2bd5.js"
+  }
+]);
